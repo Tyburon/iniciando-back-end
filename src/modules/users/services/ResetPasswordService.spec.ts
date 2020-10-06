1f92eb571d730b6996928d1e2ba14c5a9b6ae953
Gabriel Cezar <gabriel.cezar.coder@gmail.com>
2020-10-06 10:19:42 -0300

    ResetPasswordServiceCommit

---
+++ src/modules/users/services/ResetPasswordService.spec.ts
@@ -0,0 +1,43 @@
+// import AppError from '@shared/error/AppError';
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import FakeUserTokenRepository from '@modules/users/repositories/fakes/FakeUserTokenRepository';
+import ResetPasswordService from './ResetPasswordService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let fakeUserTokensRepository: FakeUserTokenRepository;
+let resetPassword: ResetPasswordService;
+
+describe('ResetPassword', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeUserTokensRepository = new FakeUserTokenRepository();
+    resetPassword = new ResetPasswordService(
+      fakeUsersRepository,
+      fakeUserTokensRepository,
+    );
+  });
+
+  it('should be able to reset the password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: '123456',
+    });
+
+    const { token } = await fakeUserTokensRepository.generate(user.id);
+
+    await resetPassword.execute({
+      password: '123123',
+      token,
+    });
+
+    const updateUser = await fakeUsersRepository.findById(user.id);
+
+    expect(updateUser?.password).toBe('123123');
+  });
+});
+
+// Hash
+// 2h expiração
+// User Token inexistente
+// User Inexistente
