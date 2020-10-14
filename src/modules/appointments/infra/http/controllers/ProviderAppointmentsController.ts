598cb8ec25dae027e5db4a15615ca74eba4d34bb
Gabriel Cezar <gabriel.cezar.coder@gmail.com>
2020-10-13 21:04:22 -0300

    ListProviderAppointmentsServiceCommit

---
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -0,0 +1,28 @@
+/* eslint-disable camelcase */
+import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+export default class ProviderAppointmentsController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const provider_id = request.user.id;
+    const { day, month, year } = request.body;
+
+    const listProviderAppointments = container.resolve(
+      ListProviderAppointmentsService,
+    );
+    // const appointmentsRepository = new AppointmentsRepository();
+    // const createAppointment = new CreateAppointmentService(
+    //   appointmentsRepository,
+    // );
+
+    const appointments = await listProviderAppointments.execute({
+      provider_id,
+      day,
+      month,
+      year,
+    });
+
+    return response.json(appointments);
+  }
+}
